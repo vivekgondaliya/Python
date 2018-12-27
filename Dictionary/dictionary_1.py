15f1c2b86b97112ebb2571c9633aaead349d2b3d
vivekgondaliya <vg13@zips.uakron.edu>
2018-12-27 13:18:53 -0800

    Definition of First capital letter word and Acronym is available

--- Dictionary/dictionary_1.py
+++ Dictionary/dictionary_1.py
@@ -5,9 +5,18 @@ data = json.load(open("data.json"))
 
 #function for retriving definition
 def retirve_definition(word):
+    word = word.lower()
+
+    #1st elif: to make sure the program returns definition of words with Capital first letters (Ex: Hello, Rain)
+    #2nd elif: to make sure the program returns definition of acronymns (Ex: USA, NFL)
+    #str.title(): returns a string which has first letter in each word is uppercase and all remaining letters are lowercase
     #check for non-existing words
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     else:
         return ("Sorry, the word doesn't exist in this dictionary.")
 

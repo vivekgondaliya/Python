472512cbede153b38769c101f7632930c850a5b6
vivekgondaliya <vg13@zips.uakron.edu>
2019-01-19 20:17:14 -0500

    Method Overidding

--- Inheritance/main.py
+++ Inheritance/main.py
@@ -5,27 +5,37 @@ class Animal:
     def eat(self):
         print("eat")
 
-#Mammal IS an Animal
+# Mammal IS an Animal
+
+
 class Mammal(Animal):
+    def __init__(self):
+        super().__init__()
+        self.weight = 14
+
     def walk(self):
         print("walk")
 
-#Fish IS an Animal
+# Fish IS an Animal
+
+
 class Fish(Animal):
     def swim(self):
         print("swim")
 
+
 mammalObj = Mammal()
 mammalObj.eat()
 print("Age: ", mammalObj.age)
+print("Weight: ", mammalObj.weight)
 
-#Probelm: We have defined eat() TWICE
+# Probelm: We have defined eat() TWICE
 #Instills: DRY
-#Why? if you have a bug, you will have to fix all methods
-#Why? if you want to modify, you will have to modify all methods
+# Why? if you have a bug, you will have to fix all methods
+# Why? if you want to modify, you will have to modify all methods
 
-#Inheritance: mechanism that defines common behavior or funtions in one class
-#             Inherit them in other classses 
-# 
+# Inheritance: mechanism that defines common behavior or funtions in one class
+#             Inherit them in other classses
+#
 # Object Class: Mammal isInstanceOf Animal and Animal isInstanceOf Object
 # i.e. Mammal isInstanceOf Object
